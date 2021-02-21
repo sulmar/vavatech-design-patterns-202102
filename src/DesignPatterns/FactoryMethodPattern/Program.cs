cde6e030b2a77ff96232dd1687f00b03183c83b6
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-02-21 16:36:52 +0100

    Initial commit

---
+++ src/DesignPatterns/FactoryMethodPattern/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace FactoryMethodPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Factory Method Pattern!");
+
+            VisitCalculateAmountTest();
+        }
+
+        private static void VisitCalculateAmountTest()
+        {
+            while (true)
+            {
+                Console.Write("Podaj rodzaj wizyty: (N)FZ (P)rywatna (F)irma: ");
+                string visitType = Console.ReadLine();
+
+                Console.Write("Podaj czas wizyty w minutach: ");
+                if (double.TryParse(Console.ReadLine(), out double minutes))
+                {
+                    TimeSpan duration = TimeSpan.FromMinutes(minutes);
+
+                    Visit visit = new Visit(duration, 100);
+
+                    decimal totalAmount = visit.CalculateCost(visitType);
+
+                    if (totalAmount == 0)
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    else
+                       if (totalAmount >= 200)
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    else
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                    Console.WriteLine($"Total amount {totalAmount:C2}");
+
+                    Console.ResetColor();
+                }
+            }
+
+        }
+    }
+
+    #region Models
+
+
+    public class Visit
+    {
+        public DateTime VisitDate { get; set; }
+        public TimeSpan Duration { get; set; }
+        public decimal PricePerHour { get; set; }
+
+        private const decimal companyDiscountPercentage = 0.9m;
+
+        public Visit(TimeSpan duration, decimal pricePerHour)
+        {
+            VisitDate = DateTime.Now;
+            Duration = duration;
+            PricePerHour = pricePerHour;
+        }
+
+        public decimal CalculateCost(string kind)
+        {
+            decimal cost = 0;
+
+            if (kind == "N")
+            {
+                cost = 0;
+            }
+            else if (kind == "P")
+            {
+                cost = (decimal)Duration.TotalHours * PricePerHour;
+            }
+            else if (kind == "F")
+            {
+                cost = (decimal)Duration.TotalHours * PricePerHour * companyDiscountPercentage;
+            }
+
+            return cost;
+        }
+    }
+
+    #endregion
+}
